2894625c2ffc82aafac7ebb09f138a7eee5e5298
7934-方露宇 <fangluyu@wondersgroup.com>
2024-04-09 15:20:31 +0800

    添加了api相关的目录。

---
+++ api/aicenterapi/src/main/java/io/afu/aicenterapi/AicenterapiApplication.java
@@ -0,0 +1,13 @@
+package io.afu.aicenterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicenterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicenterapiApplication.class, args);
+	}
+
+}
---
+++ api/aicenterapi/src/test/java/io/afu/aicenterapi/AicenterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.afu.aicenterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicenterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
